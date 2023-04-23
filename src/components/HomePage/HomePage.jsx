0ec31ef11c6f3f9b0be0300b3d84310983342f07
ValeryVVV <110850242+ValeryVVV@users.noreply.github.com>
2023-04-23 15:31:45 +0300

    fix

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Button, Link, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { styled } from '@mui/system';
 import { useNavigate } from "react-router-dom";
 
